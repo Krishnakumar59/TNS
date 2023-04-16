ff116f95ef5f36a705837e0ae5b0b283930b47a0
Krishna Kumar <Krishnakumarjnk@gmail.com>
2023-04-16 20:07:52 +0530

    First Program

---
+++ src/Day1/program1_basic_java.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class program1_basic_java {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Everyone! Krishna Here");
+        System.out.println();
+    }
+}
