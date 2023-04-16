917a9c239ddc37b11fe25c33dc38fcbd2d15e1dd
Krishna Kumar <Krishnakumarjnk@gmail.com>
2023-04-17 01:16:24 +0530

    Day 1 program solved

--- src/Day1/program1_basic_java.java
+++ src/Day1/program1_basic_java.java
@@ -2,8 +2,12 @@ package Day1;
 
 public class program1_basic_java {
     public static void main(String[] args) {
+        String batch = "M28";
+        System.out.println("Hello Everyone! \nKrishna Here");
+        System.out.println(batch);
 
-        System.out.println("Hello Everyone! Krishna Here");
-        System.out.println();
+
+
+        
     }
 }
